f5cd8e4d06a8d0419f539c6c92afad660a6d9e2f
Davide Petilli <7h3.k3rn31@gmail.com>
2016-08-19 12:19:25 +0200

    add some comments

--- lib/skuby/gateway.rb
+++ lib/skuby/gateway.rb
@@ -1,13 +1,20 @@
 module Skuby
+  # This class in the main interface with Skebby. It talks with the base_uri
+  # to send an SMS.
   class Gateway
     include HTTParty
     base_uri 'https://gateway.skebby.it/api/send/smseasy/advanced/http.php'
 
+    # Sends the SMS.
+    # - text: the text of the SMS
+    # - recipients: and array of hashes containing numbers and variable:
+    #   [{recipient: '391234567', nome: 'Nome'}, {...}]
     def self.send_sms(text = '', recipients = '')
       params = build_params(text, recipients)
       SMSResponse.new(post('', body: params), text, recipients)
     end
 
+    # Builds the parameters for the http request to Skebby.
     def self.build_params(text, recipients)
       recipients_hash = {}
       recipients.each_with_index do |recipient, index|
