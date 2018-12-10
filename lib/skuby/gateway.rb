ca0db584020229094e1971c981afb860f2cc5578
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2018-12-10 16:37:10 +0100

    Pass custom hash params to send_sms

--- lib/skuby/gateway.rb
+++ lib/skuby/gateway.rb
@@ -3,13 +3,17 @@ module Skuby
     include HTTParty
     base_uri 'https://gateway.skebby.it/api/send/smseasy/advanced/http.php'
 
-    def self.send_sms(text = '', recipients = '')
-      params = build_params(text, recipients)
+    def self.send_sms(text = '', recipients = '', custom_params = nil)
+      params = build_params(text, recipients, custom_params)
       SMSResponse.new(post('', body: params, verify: false), text, recipients)
     end
 
-    def self.build_params(text, recipients)
-      Skuby.config.to_hash.merge('text' => text, 'recipients[]' => recipients)
+    def self.build_params(text, recipients, custom_params)
+      if custom_params && custom_params.is_a?(Hash)
+        custom_params.merge('text' => text, 'recipients[]' => recipients)
+      else
+        Skuby.config.to_hash.merge('text' => text, 'recipients[]' => recipients)
+      end
     end
   end
 end
